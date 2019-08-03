46d71aaffa578c6b1bc9f0ec8c29b1fb7187dde5
Jen <jenny.quach@mail.utoronto.ca>
2019-08-03 14:02:49 -0400

    Started logic for inserting timeslots, created helper functions for traversing

--- src/timetable_generater/time_block_logic.cljs
+++ src/timetable_generater/time_block_logic.cljs
@@ -6,7 +6,14 @@
                                :column       "monday"
                                :start-time   10
                                :end-time     12
-                               :optional     {:type "lec"}}]
+                               :optional     {:type "lec"}}
+                              {:main-label   "AAAAAA"
+                               :abbreviation "AAA"
+                               :group        "AAAAAA"
+                               :column       "monday"
+                               :start-time   16
+                               :end-time     18
+                               :optional     {:type "apple"}}]
 
                    "tuesday" [{:conflict   true
                                :start-time 9                ;; ref # min items time (calculated on demand)
@@ -27,8 +34,33 @@
                                              :end-time     11
                                              :optional     {:type "lec"}}]}]})
 
+(def slot1 {:main-label   "BBBBBB"
+            :abbreviation "BBB"
+            :group        "BBBBBB"
+            :column       "monday"
+            :start-time   14
+            :end-time     16
+            :optional     {:type "apple"}})
 
-(defn insert-slot-to-column [column-slots {:keys [start-time end-time] :as slot}]
+(def slot1 {:main-label   "DD"
+            :abbreviation "D"
+            :group        "DD"
+            :column       "monday"
+            :start-time   15
+            :end-time     18
+            :optional     {:type "apple"}})
+
+(defn time-overlap? [[s1 e1] [s2 e2]]
+  (or (and (< s1 s2) (< s2 e1))                             ;; second overlaps tail of first
+      (and (< s2 s1) (< s1 e2)))                            ;; first overlaps tail of second
+  )
+
+
+
+
+(defn insert-slot-to-column [column-slots {new-slot-start :start-time
+                                           new-slot-end   :end-time
+                                           :as            new-slot}]
   )
 
 (defn add-time-slot [slots {{column :column} :required :as slot}]
--- src/timetable_generater/utils.cljs
+++ src/timetable_generater/utils.cljs
@@ -52,3 +52,52 @@
        :else %)
     identity
     template))
+
+
+(defn reduce-lazy-lookahead
+  "A reduce-like function that consumes the next 2 values and stops if the
+  result of the applied function is :break
+
+  the given function 'inner-fn' takes form of (fn [result this next] ...)
+  and returns a value or :break which stops the next consumption
+  if 'coll' has less than 2 values, reduce-lazy-lookahead returns the given 'value'
+
+  Example: Sum all consecutive even pairs
+
+  (reduce-lazy-lookahead
+    (fn [result this next]
+      (println result this next)
+      (if (and (even? this) (even? next))
+        (+ result this next)
+        result))
+    0
+    [1 2 2 3 4 5 4 2 4])
+  =>
+  16
+
+  Example: Sum all consecutive even numbers until the first non-even number
+
+  (reduce-lazy-lookahead
+    (fn [result this next]
+      (println result this next)
+      (if (and (even? this) (even? next))
+        (if (= 0 result)
+          (+ result this next)
+          (+ result next))
+        :break))
+    0
+    [6 2 2 8 3 4 5 4 2 4])
+  =>
+  18
+  "
+  ([inner-fn value coll]
+   (reduce-lazy-lookahead inner-fn value (first coll) (rest coll)))
+  ([inner-fn result this coll]
+   (if (empty? coll)
+     result
+     (let [nxt      (first coll)
+           the-rest (rest coll)
+           value    (inner-fn result this nxt)]
+       (if (= value :break)
+         result
+         (reduce-lazy-lookahead inner-fn value nxt the-rest))))))
